efe29461ee60c43becc9e190ccb60e1bcf9b3e4d
Srinandan Sridhar <nandanks@gmail.com>
2016-07-18 10:33:52 -0400

    parametrize topic; error handling

--- pubsubEvents/apiproxy/resources/jsc/get-topic-details.js
+++ pubsubEvents/apiproxy/resources/jsc/get-topic-details.js
@@ -1,5 +1,13 @@
  var payload = JSON.parse(context.getVariable("request.content"));
  
- context.setVariable("topicname", "topic1");
+ if ( ! payload.topicname) {
+   throw new Error('You must specify a topicname');
+ }
+
+ if ( ! payload.event) {
+   throw new Error('You must specify an event object to pubish');
+ }
+ 
+ context.setVariable("topicname", payload.topicname);
  
  context.setVariable("event", JSON.stringify(payload.event));
